1c36d673acf92aa4d4ab3230c8312e075bf2046f
Roxy-official <89264186+roxy-official@users.noreply.github.com>
2023-08-05 16:24:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94716315866', 'me', true],
+  ['94755228896', 'Ash', true], 
   [''] 
 ] //Number of owners
 
